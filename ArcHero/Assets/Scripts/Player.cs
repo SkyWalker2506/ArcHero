9922ced8660682af8e5952d82f2e28283facd91e
SkyWalker2506 <42008533+SkyWalker2506@users.noreply.github.com>
2020-01-11 23:10:05 +0300

    initial

---
+++ ArcHero/Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static float shotSpeed;
+    public static float shotFrequency;
+    public static float shotAmount;
+    public static GameObject PlayerCopy;
+
+    
+
+}
